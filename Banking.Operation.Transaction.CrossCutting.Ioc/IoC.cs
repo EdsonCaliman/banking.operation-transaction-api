a5ac13fe34ded1f06b3e7f41242e7a27128e175e
Edson Caliman <edsonjunior@db1.com.br>
2021-09-21 10:11:02 -0300

    Solution, folders and initial projects

---
+++ Banking.Operation.Transaction.CrossCutting.Ioc/IoC.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Banking.Operation.Transaction.CrossCutting.Ioc.Modules;
+
+namespace Banking.Operation.Transaction.CrossCutting.Ioc
+{
+    public static class IoC
+    {
+        public static IServiceCollection ConfigureContainer(this IServiceCollection services, IConfiguration configuration)
+        {
+            DataModule.Register(services, configuration);
+            services.Register();
+            return services;
+        }
+    }
+}
